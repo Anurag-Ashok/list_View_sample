79f31cba74b1f0936c57fd33949663cfceb796a8
Anurag Ashok <131293260+Anurag-Ashok@users.noreply.github.com>
2023-09-16 20:08:26 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+import 'package:gridview1/list/listView.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: list(),
+  ));
+}
